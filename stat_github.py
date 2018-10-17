fef339de89ace1b040680fcbc1e50f49eb4325c8
Monica Poelchau <mpoelchau@gmail.com>
2018-10-17 13:14:00 -0400

    Fix indentation problems

--- stat_github.py
+++ stat_github.py
@@ -13,22 +13,22 @@ def _read_git_log(repo_name):
     with open(file_name, 'r') as f:
         for line in f:
             line = line.strip()
-        if "===" in line:
-            num = line.split(" ")[1]
-            if num != "0":
-                for i in [str(cc), str(fc), str(ic), str(dc)]:
-                    result.append(i)
-            cc, fc, ic, dc = (0, 0, 0, 0)
-        else:
-            l = line.split(",")
-            cc += 1
-            for e in l:
-                if "changed" in e:
-                    fc += int(e.split(" ")[0])
-                elif "insertion" in e:
-                    ic += int(e.split("insertion")[0])
-                elif "deletion" in e:
-                    dc += int(e.split("deletion")[0])
+            if "===" in line:
+                num = line.split(" ")[1]
+                if num != "0":
+                    for i in [str(cc), str(fc), str(ic), str(dc)]:
+                        result.append(i)
+                cc, fc, ic, dc = (0, 0, 0, 0)
+            else:
+                l = line.split(",")
+                cc += 1
+                for e in l:
+                    if "changed" in e:
+                        fc += int(e.split(" ")[0])
+                    elif "insertion" in e:
+                        ic += int(e.split("insertion")[0])
+                    elif "deletion" in e:
+                        dc += int(e.split("deletion")[0])
     return result
 
 
